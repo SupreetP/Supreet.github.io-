aacdf113560dc94f8744c9de47c5f48aa4b56f0f
SupreetP <supreetchagal8431@gmail.com>
2025-05-12 18:45:00 -0500

    Updated workflow for new Next.js export method

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/Supreet.github.io',  // Replace 'repo-name' with your actual repository name
     images: {
       unoptimized: true,
     },
